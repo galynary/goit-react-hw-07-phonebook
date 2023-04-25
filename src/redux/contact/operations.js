327a5fb9422c253c19b0cd66ed86fb6986adc86a
galynary <107271972+galynary@users.noreply.github.com>
2023-04-25 10:52:50 +0300

    change

--- src/redux/contact/operations.js
+++ src/redux/contact/operations.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
-axios.defaults.baseURL = 'https://64468e3bee791e1e2902af05.mockapi.io';
+axios.defaults.baseURL = 'https://644784b350c253374426fde7.mockapi.io';
 
 export const fetchContacts = createAsyncThunk(
   'contacts/fetchAll',
